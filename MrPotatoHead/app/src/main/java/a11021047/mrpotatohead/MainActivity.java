5d1c196b2513761858be421a8bfd28bf9e019cd1
Tessa <tessawagenaar@quicknet.nl>
2017-10-30 10:31:21 +0100

    initiated project

---
+++ MrPotatoHead/app/src/main/java/a11021047/mrpotatohead/MainActivity.java
@@ -0,0 +1,13 @@
+package a11021047.mrpotatohead;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
